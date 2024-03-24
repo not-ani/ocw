edaa0a9ad369a52b659f235e799b1d0dd19c353c
Ani <108333567+not-ani@users.noreply.github.com>
2024-03-24 16:24:44 +0000

    Update SignInButton.tsx to be child

--- src/app/_components/SignInButton.tsx
+++ src/app/_components/SignInButton.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 export default function SignInButton() {
   return (
     <div className="text-[0.875rem] inline-flex items-center justify-center">
-      <Button className="h-[2.5rem]">
+      <Button className="h-[2.5rem]" asChild>
         <Link href={`/api/auth/signin`}>
           Sign In
         </Link>
