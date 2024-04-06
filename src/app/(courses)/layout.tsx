b60db531867a54292bd878d71380a4f1dab100e3
xiften <108333567+not-ani@users.noreply.github.com>
2024-04-06 14:59:06 -0600

    fix: navbar loading error

--- src/app/(courses)/layout.tsx
+++ src/app/(courses)/layout.tsx
@@ -1,24 +1,11 @@
-import { Header } from "@/components/header";
-import { Navbar } from "../_components/navbar";
-import { headers } from "next/headers";
-
 const Layout = ({
   children
 }: {
   children: React.ReactNode;
 }) => {
 
-  const heads = headers()
-
-  const pathname = heads.get('next-url')
-  console.log(pathname)
-
-  const isS = pathname?.includes("lessons")
   return (
     <div className="h-full">
-      <div className={isS ? `block` : "h-[80px] fixed inset-y-0 w-full z-50"}>
-        <Header />
-      </div>
       <main className="pt-[80px] h-full">
         {children}
       </main>
